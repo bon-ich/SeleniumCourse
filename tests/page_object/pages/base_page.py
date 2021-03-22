7331ddc0bd4304016d2ebe9f476004c9d8c12679
bonich <bon.terebon@gmail.com>
2021-03-22 09:59:00 +0200

    added screenshots attaching for faild element search

--- tests/page_object/pages/base_page.py
+++ tests/page_object/pages/base_page.py
@@ -1,5 +1,9 @@
+import allure
+import pytest
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from allure_commons.types import AttachmentType
 
 class BasePage:
     def __init__(self, driver):
@@ -7,10 +11,18 @@ class BasePage:
         self.baseurl = "http://158.101.173.161/"
 
     def find_element(self, locator, time=5):
-        return WebDriverWait(self.driver, time).until(expected_conditions.presence_of_element_located(locator), message=f"Can't find element by locator {locator}")
+        try:
+            return WebDriverWait(self.driver, time).until(expected_conditions.presence_of_element_located(locator), message=f"Can't find element by locator {locator}")
+        except Exception as e:
+            allure.attach(self.driver.get_screenshot_as_png(), name=f"find_element_error_{locator}", attachment_type=AttachmentType.PNG)
+            pytest.fail(f"{e}")
 
     def find_elements(self, locator, time=5):
-        return WebDriverWait(self.driver, time).until(expected_conditions.presence_of_all_elements_located(locator), message=f"Can't find elements by locator {locator}")
+        try:
+            return WebDriverWait(self.driver, time).until(expected_conditions.presence_of_all_elements_located(locator), message=f"Can't find elements by locator {locator}")
+        except Exception as e:
+            allure.attach(self.driver.get_screenshot_as_png(), name=f"find_elements_error_{locator}", attachment_type=AttachmentType.PNG)
+            pytest.fail(f"{e}")
     
     def goto(self, url):
         self.driver.get(url)
--- tests/page_object/pages/product_page.py
+++ tests/page_object/pages/product_page.py
@@ -5,6 +5,7 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from allure_commons.types import AttachmentType
 
 class ProductPageLocators:    
     ADD_CART_BUTTON_LOCATOR = (By.NAME, "add_cart_product")
@@ -12,8 +13,12 @@ class ProductPageLocators:
 
 class ProductPage(BasePage):
     @allure.step("Add item to cart")
-    def add_item_to_cart(self, expected_count):
+    def add_item_to_cart(self, expected_count):    
+        add_button = self.find_element(ProductPageLocators.ADD_CART_BUTTON_LOCATOR)
+        # scroll to get button at the top of the page 
+        # button can be hovered by cookies message
+        add_button.location_once_scrolled_into_view
         # add item to cart
-        self.find_element(ProductPageLocators.ADD_CART_BUTTON_LOCATOR).click()        
+        add_button.click()        
         # wait until cart counter updates
-        WebDriverWait(self.driver, 5).until(expected_conditions.text_to_be_present_in_element(ProductPageLocators.CART_QUANTITY_BADGE_LOCATOR, expected_count))    
+        WebDriverWait(self.driver, 5).until(expected_conditions.text_to_be_present_in_element(ProductPageLocators.CART_QUANTITY_BADGE_LOCATOR, expected_count))  
